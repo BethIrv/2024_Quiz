c0ea491246e6c7aae82415adae9acb603569eac0
BethIrv <144862956+BethIrv@users.noreply.github.com>
2024-06-19 07:02:45 +1200

    Made small changes, Completed Quiz

--- Base_Quiz_v2.py
+++ Base_Quiz_v2.py
@@ -1,12 +1,12 @@
 import random
 
 
-# checks users enter yes (y) or no (n)
+# Checks user enter yes or no
 def yes_no(question):
     while True:
         response = input(question).lower()
-        # check user response, question
-        # repeat if users don't enter yes/no
+        # Checks user response, question
+        # repeats if users don't enter yes/no
         if response == "yes" or response == "y":
             return "yes"
         elif response == "no" or response == "n":
@@ -39,35 +39,35 @@ Enjoy!!
     ''')
 
 
-# checks for an integer
-# lower limits and an optional exit code for infinite mode
+# Checks for an integer
+# Lower limits and an optional exit code for infinite mode
 def int_check(question, low=None, high=None, exit_code="xxx"):
     # if any integer is allowed...
     if low is None and high is None:
         error = "Please enter an integer"
-    # if the number needs to be more than an
-    # integer (ie: rounds / 'high number')
+    # If the number needs to be more than an
+    # integer (ie: questions / 'low number')
     elif low is not None and high is None:
         error = (f"Please enter an integer that is "
                  f"more than / equal to {low}")
-    # if the number needs to be between low and high
+    # If the number needs to be between low and high
     else:
         error = (f"Please enter an integer that"
                  f" is between {low} and {high} (inclusive)")
     while True:
         response = input(question).lower()
-        # check for infinite mode / exit code
+        # Checks for infinite mode / exit code
         if response == exit_code:
             return response
         try:
             response = int(response)
-            # Check the integer is not too low...
+            # Checks the integer is not too low
             if low is not None and response < low:
                 print(error)
-            # check response is more than the low number
+            # Checks response is more than the low number
             elif high is not None and response > high:
                 print(error)
-            # if the response is valid, return it
+            # If the response is valid, returns it
             else:
                 return response
         except ValueError:
@@ -80,14 +80,14 @@ def string_checker(question, valid_ans=("yes", "no")):
         # Get user response and make sure it's lowercase
         user_response = input(question).lower()
         for item in valid_ans:
-            # check if the user response is a word in the list
+            # Check if the user response is a word in the list
             if item == user_response:
                 return item
-            # check if the user response is the same as
+            # Checks if the user response is the same as
             # the first letter of an item in the list
             elif user_response == item[0]:
                 return item
-        # print error if user does not enter something that is valid
+        # Prints error if user does not enter something that is valid
         print(error)
         print()
 
@@ -101,9 +101,9 @@ print()
 print("😸😸 Basic Facts Quiz 😸😸")
 print()
 
-# asking user if they want to read instructions
+# Asks user if they want to read instructions
 want_instructions = yes_no("Would you like to read the instructions? ")
-# check is users enter yes (y) or no (n)
+# Checks if user enters yes or no
 if want_instructions == "yes":
     instructions()
 
@@ -123,16 +123,16 @@ quiz_history = []
 history = []
 operation_list = ["addition", "subtraction", "multiplication", "division", "mix"]
 
-# ask user what type of operations they would like
-# includes the option to have mixed operations
+# Ask user what type of operations they would like to use
+# Includes the option to have mixed operations
 operation_type = string_checker("What type of operation would you like? "
                                 "(Addition, Subtraction, Multiplication, Division, Mix) ", operation_list)
 
-# ask user what difficulty level they would like
+# Asks user what difficulty level they would like to play
 difficulty_level = int_check("What level difficulty would you like your questions to be? (Choose between levels 1-3): ",
                              low=1, high=3)
 
-# loops while question_num is lower than amount of questions - chosen at the start
+# Loops while question_number is lower than amount of questions, is chosen at the start
 # Quiz question loop starts here
 while question_number < number_questions:
 
@@ -152,11 +152,11 @@ while question_number < number_questions:
     elif difficulty_level == 3:
         num_1 = random.randint(1, 50)
         num_2 = random.randint(1, 50)
-    # generates a number to make sure the correct answer is an integer
+    # Generates a number, makes sure the correct answer is an integer
     num_3 = num_1 * num_2
     num_4 = num_1 + num_2
     random_bf_list = ["addition questions", "subtraction questions", "multiplication questions", "division questions"]
-    # generates the question type
+    # Generates the operation type
     if operation_type == "addition":
         question = "addition questions"
     elif operation_type == "subtraction":
@@ -172,7 +172,7 @@ while question_number < number_questions:
     already_tried = []
     attempts_allowed = 3
     user_choice = ""
-    # generates the question format
+    # Generates the question format
     if question == "addition questions":
         answer = num_1 + num_2
         question_form = f"What's {num_1} + {num_2} ? "
@@ -195,14 +195,14 @@ while question_number < number_questions:
 
     while user_choice != answer and user_chose < attempts_allowed:
         user_choice = int_check(question_form)
-        # check that they don't want to quit
+        # Checks to see if user doesn't want to quit
         if user_choice == "xxx":
-            # set end_game to use that outer loop can be broken
+            # Sets end_game to use that outer loop can be broken
             end_game = "yes"
             break
-        # add one to the number of guesses used
+        # Adds one to the amount of attempts already used
         user_chose += 1
-        # checks if user has already tried their answer before
+        # Says if user has tried this answer before
         if user_choice in already_tried:
             user_chose += -1
             print(f"You've already tried {user_choice}. You've used {user_chose} / {attempts_allowed} attempts")
@@ -210,18 +210,18 @@ while question_number < number_questions:
         else:
             already_tried.append(user_choice)
 
-        # allows user to try to answer the question again until they have run out of chances
+        # Lets user have attempts at answering the question
         if user_choice != answer and user_chose < attempts_allowed:
             print(f"Incorrect!🤦‍♀️ Try again! You have used {user_chose} / {attempts_allowed} attempts")
 
-        # feedback if user was unable to answer the question correctly
+        # Gives feedback for any incorrect answers
         elif user_choice != answer and user_chose == attempts_allowed:
             feedback = "Sorry, you did not answer correctly. " \
                        f"The answer was {answer}"
             question_incorrect += 1
             history.append(feedback)
 
-        # gives feedback if they answer correctly
+        # Gives feedback for the correct answers
         else:
             if user_choice == answer and user_chose == 1:
                 feedback = "Good Job! You got the answer in one go!"
@@ -235,25 +235,25 @@ while question_number < number_questions:
             elif user_choice == "xxx":
                 break
 
-    # print feedback to user
+    # Prints feedback for user to see
     if end_game == "no":
         print(feedback)
 
     elif end_game == "yes":
         break
 
-    # history and feedback
+    # Shows history and feedback
     history_item = f"Question {question_number + 1}: {feedback}"
     quiz_history.append(history_item)
 
     question_number += 1
 
-    # if users are in infinite mode, increase number of questions!
+    # If users are in infinite mode, will increase questions
     if mode == "infinite":
         number_questions += 1
 
-# quiz history / statistics area
-# calculate statistics
+# Quiz history / statistics area
+# Calculates the statistics
 if question_number > 0:
     question_correct = question_number - question_incorrect
     amount_correct = question_correct / question_number * 100
@@ -261,10 +261,10 @@ if question_number > 0:
 
     print()
     print("✨✨ Quiz Statistics ✨✨")
-    print(f"👍 Questions you got correct: {amount_correct:.2f} 👍 \t")
-    print(f"👎 Questions you got incorrect: {amount_incorrect:.2f} 👎 \t")
+    print(f"👍 Questions you got correct: {amount_correct:.2f}% 👍 \t")
+    print(f"👎 Questions you got incorrect: {amount_incorrect:.2f}% 👎 \t")
 
-    # ask user if they want to see their game history and output it if requested.
+    # Asks user if they want to see history, shows if yes
     see_history = yes_no("\nDo you want to see your quiz history? ")
     if see_history == "yes":
         for item in quiz_history:
